839aebfde1ae6c09cb2a5ec9312fddd1b509ea4f
Bryan Thornlow <bthornlo@ucsc.edu>
2023-04-09 15:22:38 -0400

    Add files via upload

--- getMFE.py
+++ getMFE.py
@@ -1,50 +1,24 @@
 #!/usr/bin/env python3
-# Name: Bryan Thornlow
-# Date: 6/6/2017
-# getMFE.py
+import argparse
 
-import sys
-import os
-import time
-import random
-import numpy
-import gzip
-import math
 
-"""
-This program converts creates a file to be analyzed by RNAfold.
-To do so, plug the file output by this program into this command:
-
-RNAfold --noPS -C < (output from this script) > tRNA.mfe
-"""
-
-import sys, argparse, random, math
-
-class CommandLine(object):
+class CommandLine:
     """Handles the input arguments from the command line. Manages 
     the argument parser.
-
     Methods:
     Other than initialization, no methods are present, as its purpose is 
     simply to handle what is passed into the command line and pass that 
     into the class that performs the searching algorithm."""
 
-    def __init__(self, inOpts=None):
-        '''
-        CommandLine constructor.
-        Implements a parser to interpret the command line input using argparse.
-        '''
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("-s", "--ssFile", help="Input .ss"+
-            " of high-confidence tRNAs from tRNAscan-SE 2.0.")
-        self.parser.add_argument("-b", "--bedFile", help="Input .bed"+
-            " of high-confidence tRNAs from tRNAscan-SE 2.0.")
-        self.parser.add_argument("-o", "--outFile", help="File to be analyzed"+
-            " by RNAfold to get MFE data for tRNAs.", default='tRNAFoldIn.txt')
-        self.args = self.parser.parse_args()
+    def __init__(self):
+        parser = argparse.ArgumentParser()
+        parser.add_argument("-s", "--ssFile", help="Input .ss of high-confidence tRNAs from tRNAscan-SE 2.0.")
+        parser.add_argument("-b", "--bedFile", help="Input .bed of high-confidence tRNAs from tRNAscan-SE 2.0.")
+        parser.add_argument("-o", "--outFile", help="File to be analyzed by RNAfold to get MFE data for tRNAs.", default='tRNAFoldIn.txt')
+        self.args = parser.parse_args()
 
 
-class fileConverter(object):
+class FileConverter:
     """
     Primary class where filetype is converted.
     """
@@ -54,67 +28,48 @@ class fileConverter(object):
         self.bedFile = bedFile
         self.outFile = outFile
 
-    def processStr(self, myString):
-        toReturn = ''
-        for i in range(0,len(myString)):
-            if myString[i] == '>':
-                toReturn += '('
-            elif myString[i] == '.':
-                toReturn += '.'
-            elif myString[i] == '<':
-                toReturn += ')'
-        return(toReturn)
+    def process_str(self, my_string):
+        return my_string.replace('>', '(').replace('.', '.').replace('<', ')')
 
-    def convertFile(self):
-        myOutString = ''
-        coordTotRNA = {}
-        for line in open(self.bedFile):
-            splitLine = (line.strip()).split('\t')
-            for k in range(int(splitLine[1]),int(splitLine[2])+1):
-                coordTotRNA[splitLine[0]+'__'+str(k)] = str(splitLine[3])
-        mytRNA = ''
-        for line in open(self.ssFile):
-            splitLine = (line.strip()).split()
-            # first line
-            if len(splitLine) > 2 and 'Length' in splitLine[2]:
-                myChrom = splitLine[0].split('.trna')[0]
-                myCoords = ((splitLine[1])[1:-1]).split('-')
-                myStart = min(int(myCoords[0]), int(myCoords[1]))
-                myEnd = min(int(myCoords[0]), int(myCoords[1]))
-                #print(myStart, myEnd)
-                if myChrom+'__'+str(myEnd+25) in coordTotRNA:
-                    mytRNA = coordTotRNA[myChrom+'__'+str(myEnd+25)]
-            # seq line
-            if len(mytRNA) > 1 and (splitLine[0]).startswith('Seq:'):
-                mySeq = (splitLine[1]).upper()
-            if len(mytRNA) > 1 and (splitLine[0]).startswith('Str:'):
-                myOutString += '>'+mytRNA+'\n'+mySeq+'\n'+self.processStr(splitLine[1])+'\n'
-                mytRNA = ''
-        open(self.outFile, 'w').write(myOutString)
+    def convert_file(self):
+        coord_to_tRNA = {}
+        with open(self.bedFile) as f:
+            for line in f:
+                split_line = line.strip().split('\t')
+                for k in range(int(split_line[1]), int(split_line[2]) + 1):
+                    coord_to_tRNA[f'{split_line[0]}__{k}'] = str(split_line[3])
+        out_string = ''
+        with open(self.ssFile) as f:
+            tRNA = ''
+            for line in f:
+                split_line = line.strip().split()
+                if len(split_line) > 2 and 'Length' in split_line[2]:
+                    chrom = split_line[0].split('.trna')[0]
+                    coords = ((split_line[1])[1:-1]).split('-')
+                    start, end = min(int(coords[0]), int(coords[1]))
+                    if f'{chrom}__{end + 25}' in coord_to_tRNA:
+                        tRNA = coord_to_tRNA[f'{chrom}__{end + 25}']
+                if len(tRNA) > 1 and split_line[0].startswith('Seq:'):
+                    seq = split_line[1].upper()
+                if len(tRNA) > 1 and split_line[0].startswith('Str:'):
+                    out_string += f'>{tRNA}\n{seq}\n{self.process_str(split_line[1])}\n'
+                    tRNA = ''
+        with open(self.outFile, 'w') as f:
+            f.write(out_string)
 
 
-def main(myCommandLine=None):
+def main():
     """
     Initializes a CommandLine object and passes the provided 
-    arguments into a new fileConverter object and calls main method.
+    arguments into a new FileConverter object and calls main method.
     """
-    myCommandLine = CommandLine()
+    command_line = CommandLine()
+    file_converter = FileConverter(command_line.args.ssFile, command_line.args.bedFile, command_line.args.outFile)
+    file_converter.convert_file()
 
-    if myCommandLine.args.ssFile:
-        ssFile = myCommandLine.args.ssFile
-
-    if myCommandLine.args.bedFile:
-        bedFile = myCommandLine.args.bedFile
-
-    if myCommandLine.args.outFile:
-        outFile = myCommandLine.args.outFile
-
-    myFileConverter = fileConverter(ssFile, bedFile, outFile)
-    myFileConverter.convertFile()
 
 if __name__ == "__main__":
     """
     Calls main when program is run by user.
     """
-    main();
-    raise SystemExit
+    main()
--- reduce4d.py
+++ reduce4d.py
@@ -1,110 +1,29 @@
 #!/usr/bin/env python3
-# Name: Bryan Thornlow
-# Date: 6/6/2017
-# gffToBed.py
-
-import sys
-import os
-import time
-import random
-import numpy
-import gzip
-import math
-from random import shuffle
+import argparse
 import operator
+import random
 
-"""
-This program grabs a set number of exons from a bed file of 4d sites
-to make a smaller file more easily used to train a PhyloP model.
-"""
+def parse_args():
+    parser = argparse.ArgumentParser(description='Reduce 4D exons from BED file')
+    parser.add_argument('-i', '--inputFile', required=True, help='Input BED file of 4D sites')
+    parser.add_argument('-o', '--outputFile', help='Output file path')
+    parser.add_argument('-n', '--numExons', type=int, default=100000, help='Number of exons to include')
+    return parser.parse_args()
 
-import sys, argparse, random, math
+def reduce_4d_exons(input_file, output_file, num_exons):
+    with open(input_file) as f:
+        lines = f.readlines()
 
-class CommandLine(object):
-    """Handles the input arguments from the command line. Manages 
-    the argument parser.
+    random.shuffle(lines)
+    keep_lines = sorted(lines[:num_exons], key=operator.itemgetter(0, 1))
 
-    Methods:
-    Other than initialization, no methods are present, as its purpose is 
-    simply to handle what is passed into the command line and pass that 
-    into the class that performs the searching algorithm."""
+    with open(output_file, 'w') as f:
+        f.writelines(keep_lines)
 
-    def __init__(self, inOpts=None):
-        '''
-        CommandLine constructor.
-        Implements a parser to interpret the command line input using argparse.
-        '''
-        self.parser = argparse.ArgumentParser()
-        self.parser.add_argument("-i", "--inputFile", help="Input .bed"+
-            " of 4d sites to reduce.")
-        self.parser.add_argument("-o", "--outputFile", help="The path to"+
-            " your desired output file.", default='')
-        self.parser.add_argument("-n", "--numExons", help="Number of"+
-            " exons to include in the file.", default=100000)
-        self.args = self.parser.parse_args()
+def main():
+    args = parse_args()
+    output_file = args.outputFile or f'{args.inputFile.split(".")[0]}_reduced.bed'
+    reduce_4d_exons(args.inputFile, output_file, args.numExons)
 
-class fileConverter(object):
-    """
-    Primary class where filetype is converted.
-    """
-
-    def __init__(self, inputFile, outputFile, numExons):
-        self.inputFile = inputFile
-        self.outputFile = outputFile
-        self.numExons = numExons
-
-    def reduce4dBeds(self):
-        allLines = []
-        keepLines = []
-        myOutString = ''
-        for line in open(self.inputFile):
-            allLines.append((line.strip()).split('\t'))
-        shuffle(allLines)
-        for i in range(0,self.numExons):
-            keepLines.append(allLines[i])
-        for k in sorted(keepLines, key=operator.itemgetter(0,1)):
-            myOutString += joiner(k)+'\n'
-        open(self.outputFile, 'w').write(myOutString)
-
-def joiner(entry):
-    newList = []
-    for k in entry:
-        newList.append(str(k))
-    return '\t'.join(newList)
-
-
-def main(myCommandLine=None):
-    """
-    Initializes a CommandLine object and passes the provided 
-    arguments into a new fileConverter object and calls main method.
-    """
-    myCommandLine = CommandLine()
-
-    if myCommandLine.args.inputFile:
-        inputFile = myCommandLine.args.inputFile
-
-    if myCommandLine.args.outputFile:
-        outputFile = myCommandLine.args.outputFile
-
-    if len(myCommandLine.args.outputFile) == 0:
-        outputFile = inputFile.split('.')[0]+'reduced.bed'
-
-    if myCommandLine.args.numExons:
-        numExons = int(myCommandLine.args.numExons)
-
-    myFileConverter = fileConverter(inputFile, outputFile, numExons)
-    myFileConverter.reduce4dBeds()
-
-if __name__ == "__main__":
-    """
-    Calls main when program is run by user.
-    """
-    main();
-    raise SystemExit
-
-
-
-
-
-
-
+if __name__ == '__main__':
+    main()
